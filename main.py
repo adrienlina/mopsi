c2732443a93b62f99b3b7fe876671b8cd24c5cee
Adrien Lina <adrienlina@gmail.com>
2016-03-09 19:10:26 +0100

    Added barplot of the results at the end of the app

--- main.py
+++ main.py
@@ -4,19 +4,24 @@ import sys
 from pygame.locals import *
 from random import randint
 import time
+from barplot import ResultBarPlot
 
 
 class App:
 
-    def __init__(self, n=10, dt=0, dx=20):
+    def __init__(self, n=20, dt=0, dx=10):
         self.n = n
+        self.m = n * n
         self.dt = dt
         self.grid = [[False for i in range(0, 2 * n)] for j in range(0, 2 * n)]
         self.reset_grid()
 
         self._running = True
         self.w = self.h = dx
-        self.size = 2 * n * self.w + 100, 2 * n * self.h
+        self.size = 2 * n * self.w, 2 * n * self.h
+
+        self.second_screen = 2 * n * self.w
+
         self.screen = pygame.display.set_mode(self.size)
 
         self.blackSquare = pygame.Surface((self.w, self.w))
@@ -62,29 +67,31 @@ class App:
 
         self.reset_display()
         clock = pygame.time.Clock()
-        start = pygame.time.get_ticks()
+        counter = 0
         while self._running:
             clock.tick(self.dt)
             self.random_flip()
+            counter += 1
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self._running = False
                     break
                 if event.type == pygame.KEYUP:
                     if event.key == K_SPACE:
-                        self.results.append(pygame.time.get_ticks() - start)
+                        self.results.append(counter)
+                        counter = 0
                         self.reset_grid()
                         self.reset_display()
-                        start = pygame.time.get_ticks()
                     elif event.key == K_q:
                         self._running = False
                         break
             pygame.display.flip()
 
-        for result in self.results:
-            print result
-
         pygame.quit()
+
+        result_barplot = ResultBarPlot()
+        result_barplot.results = self.results
+        result_barplot.plot()
 
 
 if __name__ == "__main__" :
